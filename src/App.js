0fc5e96f023047efd259228e52e333747a943e25
Neil <barrymun@tcd.ie>
2020-03-26 12:04:03 +0000

    converting variables to state

--- src/App.js
+++ src/App.js
@@ -2,48 +2,56 @@ import React from 'react';
 import * as THREE from "three";
 import {Base} from "./_components";
 
-const EARTH_PATH = "images/land_ocean_ice_cloud_2048.jpg";
+const EARTH_PATH = "images/earth2048.jpg";
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 const VIEW_ANGLE = 45;
 const ASPECT = WIDTH / HEIGHT;
 const NEAR = 0.1;
 const FAR = 10000;
+const RADIUS = 200;
+const SEGMENTS = 50;
+const RINGS = 50;
 
 class App extends Base {
 
   container = null;
-  renderer = null;
-  scene = null;
-  camera = null;
-  globe = null;
+
+  state = {
+    renderer: null,
+    scene: null,
+    camera: null,
+    globe: null,
+  };
+
 
   constructor(props) {
     super(props);
 
+    // refs
     this.container = React.createRef();
 
+    // bindings
     this.animate = this.animate.bind(this);
   }
 
 
-  componentDidMount() {
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(WIDTH, HEIGHT);
+  async componentDidMount() {
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(WIDTH, HEIGHT);
 
-    this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-    this.camera.position.set(0, 0, 300);
+    // append the renderer to the DOM
+    this.container.appendChild(renderer.domElement);
 
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x000);
-    this.scene.add(this.camera);
-    this.container.appendChild(this.renderer.domElement);
+    let camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+    camera.position.set(0, 0, 300);
 
-    const RADIUS = 200;
-    const SEGMENTS = 50;
-    const RINGS = 50;
-    this.globe = new THREE.Group();
-    this.scene.add(this.globe);
+    let scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000);
+    scene.add(camera);
+
+    let globe = new THREE.Group();
+    scene.add(globe);
 
     let loader = new THREE.TextureLoader();
     loader.load(
@@ -58,32 +66,58 @@ class App extends Base {
         let mesh = new THREE.Mesh(sphere, material);
 
         // Add mesh to globe
-        this.globe.add(mesh);
+        globe.add(mesh);
 
       },
       null,
       err => {
         console.log({err})
       });
-    this.globe.position.z = -300;
 
-    const pointLight = new THREE.PointLight(0xFFFFFF);
+    // set initial position
+    globe.position.z = -300;
+
+    let pointLight = new THREE.PointLight(0xFFFFFF);
     pointLight.position.x = 10;
     pointLight.position.y = 50;
     pointLight.position.z = 400;
-    this.scene.add(pointLight);
+    scene.add(pointLight);
 
-    this.animate()
+    await this.setStateAsync({
+      renderer,
+      scene,
+      camera,
+      globe,
+    });
+
+    this.animate();
   }
 
 
-  animate() {
+  /**
+   *
+   */
+  async animate() {
+    let {
+      renderer,
+      scene,
+      camera,
+      globe,
+    } = this.state;
+
     // render:
-    this.renderer.render(this.scene, this.camera);
+    renderer.render(scene, camera);
 
     // rotate
-    this.globe.rotation.y += 0.003;
-    // this.globe.rotation.x += 0.01;
+    globe.rotation.y += 0.003;
+    // globe.rotation.x += 0.01;
+
+    await this.setStateAsync({
+      renderer,
+      scene,
+      camera,
+      globe,
+    });
 
     // schedule the next frame:
     requestAnimationFrame(this.animate);
--- src/_components/Base.js
+++ src/_components/Base.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 class Base extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.setStateAsync = this.setStateAsync.bind(this);
+  }
+
+
+  /**
+   * allows for "await" kw when state is being set to avoid
+   * unreadable callback code
+   *
+   * @param state
+   * @returns {Promise<any>}
+   */
+  setStateAsync(state) {
+    return new Promise(resolve => this.setState(state, resolve));
+  };
+
 }
 
 export {Base};
